5603076263c68790659e6e571ff10a5bfd5e25e0
Kweayon  Clark <Kweayon@gmail.com>
2022-12-09 13:27:28 -0600

    Updates

--- src/index.js
+++ src/index.js
@@ -3,8 +3,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./projects/sorting_components/app";
+import ControledComponent from "./projects/form_practice/controled_component/ControledComponent";
 
 const _id = document.getElementById("root");
 const root = ReactDOM.createRoot(_id);
 
-root.render(<App />);
+root.render(<ControledComponent />);
