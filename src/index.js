7a397cec80e73511e8dfe536f93d6869119de5b5
Kweayon  Clark <Kweayon@gmail.com>
2022-12-17 17:36:56 -0600

    Update

--- src/index.js
+++ src/index.js
@@ -3,9 +3,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 // import App from "./projects/sorting_components/app";
-import ControledComponent from "./projects/form_practice/controlled_component/ControlledComponent";
-
+// import ControledComponent from "./projects/form_practice/controlled_component/ControlledComponent"; <ControledComponent />
+// import StyledComponents1 from "./projects/styled_components/style1"; <StyledComponents1 />
+import { StyledComponents2 } from "./projects/styled_components/style2";
 const _id = document.getElementById("root");
 const root = ReactDOM.createRoot(_id);
 
-root.render(<ControledComponent />);
+root.render(<StyledComponents2 />);
