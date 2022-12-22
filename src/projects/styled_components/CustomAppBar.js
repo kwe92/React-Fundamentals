fd83609f5f693a6a78379b9139f5c9638fcf173d
Kweayon  Clark <Kweayon@gmail.com>
2022-12-22 15:15:23 -0600

    Custom AppBar Styled Component

--- src/projects/styled_components/CustomAppBar.js
+++ src/projects/styled_components/CustomAppBar.js
@@ -1,44 +1,80 @@
-import styled from "@emotion/styled";
 import { GiHamburgerMenu } from "react-icons/gi";
-import React from "react";
-
-const AppBar = styled.div`
-  height: 56px;
-  background-color: black;
-  position: absolute;
-  display: flex;
-  align-items: center;
-  top: 0;
-  bottom: 0;
-  left: 0;
-  right: 0;
-  box-shadow: 1px 2px 4px grey;
-`;
-
-const StyledLogo = styled.div`
-  white-space: nowrap;
-  xs: none;
-  sm: block;
-`;
+import React, { useState } from "react";
+import { AiOutlineSearch } from "react-icons/ai";
+import Switch from "@mui/material/Switch";
+import FormGroup from "@mui/material/FormGroup";
+import FormControlLabel from "@mui/material/FormControlLabel";
+import AccountCircle from "@mui/icons-material/AccountCircle";
+import { Typography } from "@mui/material";
+import * as styles from "./appBarStyles";
 
 const pages = [
   { label: "MOVIES", route: "" },
-  { label: "TV SHOWS", route: "tvshows" },
-  { label: "TRENDING", route: "trending" },
+  { label: "TV SHOWS", route: "bigMind" },
+  { label: "TRENDING", route: "void" },
   { label: "PRICING", route: "pricing" },
 ];
 
-export const StyledComponents3 = () => {
+const buttonLinks = pages.map((page) => {
   return (
-    <AppBar>
-      <GiHamburgerMenu
-        style={{ color: "white", padding: "0rem 0.5rem" }}
-      ></GiHamburgerMenu>
-      <StyledLogo>
-        <h4 style={{ color: "white" }}>VueMix</h4>
-      </StyledLogo>
+    <styles.StyledNavLink to={page.route}>{page.label}</styles.StyledNavLink>
+  );
+});
 
-      <div></div>
-    </AppBar>
+export const StyledAppBar = () => {
+  const [userInput, setUserInput] = useState("");
+
+  const handleUserInput = (event) => {
+    const input = event.target.value;
+    setUserInput(input);
+  };
+  return (
+    <styles.AppBar>
+      <GiHamburgerMenu
+        style={{ color: "white", padding: "0rem 1rem" }}
+      ></GiHamburgerMenu>
+
+      {/* Logo Start */}
+      <styles.StyledLogo>
+        <styles.BaseStyledNavLink to="">
+          <h4 name="title">VueMix</h4>
+        </styles.BaseStyledNavLink>
+      </styles.StyledLogo>
+
+      {/* NavLinks Start */}
+      <styles.NavLinkContainer>{buttonLinks}</styles.NavLinkContainer>
+
+      {/* Search Icon && Input Start */}
+      <styles.Wrapper>
+        <styles.StyledInputContainer>
+          <AiOutlineSearch
+            style={{ paddingLeft: "0.5rem", color: "white" }}
+          ></AiOutlineSearch>
+          <styles.StyledInput
+            placeholder="Search…"
+            inputProps={{ "aria-label": "search" }}
+            onChange={handleUserInput}
+            value={userInput}
+          ></styles.StyledInput>
+        </styles.StyledInputContainer>
+      </styles.Wrapper>
+      <div style={{ flexGrow: 1 }}>
+        {/* Switch Start */}
+        <FormGroup>
+          <FormControlLabel
+            control={<Switch onClick={() => {}} size="small" />}
+            label={
+              <Typography style={{ color: "white", fontSize: "0.75rem" }}>
+                Mode
+              </Typography>
+            }
+          />
+        </FormGroup>
+      </div>
+
+      <AccountCircle
+        style={{ color: "white", marginRight: "1rem", fontSize: "1rem" }}
+      />
+    </styles.AppBar>
   );
 };
