8da31c3cd3bd7d12e6bd37896adeef3face8b783
Kweayon  Clark <Kweayon@gmail.com>
2022-12-22 15:15:33 -0600

    Updates

--- src/index.js
+++ src/index.js
@@ -5,10 +5,24 @@ import ReactDOM from "react-dom/client";
 // import App from "./projects/sorting_components/app";
 // import ControledComponent from "./projects/form_practice/controlled_component/ControlledComponent"; <ControledComponent />
 // import StyledComponents1 from "./projects/styled_components/style1"; <StyledComponents1 />
-import { StyledComponents2 } from "./projects/styled_components/style2";
-import { StyledComponents3 } from "./projects/styled_components/style3";
+// import { StyledComponents2 } from "./projects/styled_components/style2";
+import { StyledAppBar } from "./projects/styled_components/CustomAppBar";
+import { BrowserRouter } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
+import { Page1 } from "./projects/styled_components/pages/page1";
+import { Page2 } from "./projects/styled_components/pages/page2";
+import { Page3 } from "./projects/styled_components/pages/page3";
 
-const _id = document.getElementById("root");
-const root = ReactDOM.createRoot(_id);
+const ele = document.getElementById("root");
+const root = ReactDOM.createRoot(ele);
 
-root.render(<StyledComponents2 />);
+root.render(
+  <BrowserRouter>
+    <StyledAppBar />
+    <Routes>
+      <Route path="" element={<Page1 />}></Route>
+      <Route path="bigMind" element={<Page2 />}></Route>
+      <Route path="void" element={<Page3 />}></Route>
+    </Routes>
+  </BrowserRouter>
+);
