1f794094bc930c235e566277233dae1d1f09eba2
Kweayon  Clark <Kweayon@gmail.com>
2022-12-24 22:41:52 -0600

    Refactor

--- src/projects/styled_components/appBarStyles.js
+++ src/projects/styled_components/appBarStyles.js
@@ -1,6 +1,7 @@
-import styled from "@emotion/styled";
+import styled, { css } from "styled-components";
 import { NavLink } from "react-router-dom";
 import AccountCircle from "@mui/icons-material/AccountCircle";
+import { HiDotsVertical } from "react-icons/hi";
 import { GiHamburgerMenu } from "react-icons/gi";
 
 const borderRadius = 0.25;
@@ -11,9 +12,6 @@ export const AppBar = styled.div`
   position: absolute;
   display: flex;
   flex-grow: 1;
-  // width: 100vw;
-  // padding: 0;
-  // margin: 0;
   align-items: center;
   top: 0;
   bottom: 0;
@@ -26,14 +24,21 @@ export const StyledLogo = styled.div`
   white-space: nowrap;
   color: white;
   padding-left: 2rem;
-
   h4 {
     text-size-adjust: 10%;
-    // font-size: 1.75rem;
-    font-size: 2.33vw;
+    font-size: 2.15rem;
   }
 
   @media (max-width: 870px) {
+    flex-grow: 0.5;
+  }
+
+  @media (max-width: 650px) {
+    padding-left: 0rem;
+    padding-right: 1rem;
+  }
+
+  @media (max-width: 600px) {
     display: none;
   }
 `;
@@ -43,14 +48,8 @@ export const NavLinkContainer = styled.div`
   gap: 2.5rem;
   padding-left: 2rem;
   padding-right: 2rem;
-
-  @media (max-width: 640px) {
+  @media (max-width: 870px) {
     display: none;
-  }
-
-  @media (min-width: 950px) {
-    gap: 4rem;
-    flex-grow: 1;
   }
 `;
 
@@ -77,7 +76,12 @@ export const Wrapper = styled.div`
   margin: 0rem 0rem 0rem 0rem;
   position: relative;
   background: white;
+  flex-grow: 1;
   border-radius: ${borderRadius * 2 + "rem"};
+  width: 12rem;
+  @media (max-width: 870px) {
+    width: 12rem;
+  }
   @media (max-width: 640px) {
     width: 100%;
   }
@@ -89,15 +93,10 @@ export const StyledInputContainer = styled.div`
   gap: 0.5rem;
   border-radius: ${borderRadius + "rem"};
   align-items: center;
-  width: 16rem;
 
-  @media (max-width: 820px) {
-    width: 14rem;
-  }
-
-  @media (max-width: 820px) {
-    width: 10rem;
-  }
+  // @media (max-width: 820px) {
+  //   width: 14rem;
+  // }
 
   @media (max-width: 640px) {
     width: 100%;
@@ -129,22 +128,37 @@ export const StyledInput = styled.input`
   }
 `;
 
-export const StyledAccountCircle = styled(AccountCircle)`
+const baseIcon = css`
   color: white;
-  margin-right: 3rem;
   font-size: 2rem;
-
-  @media (max-width: 450px) {
-    margin-right: 1rem;
+  flex-grow: 0.75;
+  padding-right: 1rem;
+  @media (max-width: 325px) {
+    padding-left: 1rem;
   }
+`;
+
+export const StyledAccountCircle = styled(AccountCircle)`
+  ${baseIcon}
+`;
+
+export const StyledHiDotsVertical = styled(HiDotsVertical)`
+  ${baseIcon} // font-size: 1.5rem;
 `;
 
 export const StyledGiHamburgerMenu = styled(GiHamburgerMenu)`
   color: white;
   display: none;
   font-size: 2rem;
-  @media (max-width: 450px) {
+  @media (max-width: 870px) {
     padding: 0rem 1rem;
     display: inline;
+  }
+`;
+
+export const SwitchWrapper = styled.div`
+  padding-left: 2rem;
+  @media (max-width: 325px) {
+    display: none;
   }
 `;
